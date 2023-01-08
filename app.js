2c4044b8e0ea46f42e4fc71bb09f5ee95a1db6d5
Mark Wlodawski <imakethesites@gmail.com>
2023-01-08 15:12:29 -0500

    Adding Global error handling middleware

--- app.js
+++ app.js
@@ -25,10 +25,16 @@ app.use('/api/v1/users', userRouter);
 // 4) START SERVER
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
 });
 
 app.use((err, req, res, next) => {
