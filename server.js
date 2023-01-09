afae6507f8a59789098df69bcc338bb9c7bae853
Mark Wlodawski <imakethesites@gmail.com>
2023-01-09 15:28:39 -0500

    Adding solution for errors outside express

--- server.js
+++ server.js
@@ -20,6 +20,22 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  // console.log(err.name, err.message);
+  // console.log('UNHANDLED REJECTION! Shutting Down...');
+  const fullMessage = err.message;
+  const errmsgStart = 0; // Start at the beginnning
+  const newline = /\n/; // new line character
+  const errmsgStop = fullMessage.search(newline); // Find new line
+  const errmsgLen = errmsgStop - errmsgStart;
+  const errorText = fullMessage.substr(errmsgStart, errmsgLen);
+  console.log(`💥Error Name💥: ${err.name}`);
+  console.log(`💥💥Error Text: ${errorText}`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
